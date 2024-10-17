deffbca01833846791023742b8a0c40f78dac597
unknown <dilorenzo@itis-molinari.eu>
2024-10-17 08:24:20 +0200

    codice per generare una form

---
+++ form.js
@@ -0,0 +1,23 @@
+const createForm = (parentElement) => {
+    let data;
+    let callback = null;
+  
+    return {  
+      setLabels: (labels) => { data = labels; },  
+      onsubmit: (callbackInput) => { callback = callbackInput},
+      render: () => { 
+        parentElement.innerHTML = 
+          data.map((name, index) => {
+              return `<div>${name}\n<input id="${name}" type="text" /></div>`;
+            }).join('\n')
+            + "<button type='button' id='submit'>Submit</button>";  
+        document.querySelector("#submit").onclick = () => {
+          const result = data.map((name) => {
+            return document.querySelector("#" + name).value;
+          });
+          callback(result);
+        }          
+      },
+    };
+  };
+  
